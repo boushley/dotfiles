3ca8140cd2835280ba86a79259cea003dd7061f1
Aaron Boushley <boushley@gmail.com>
2013-10-31 11:22:11 -0400

    Converting to dotfiles instead of .dotfiles.

--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
 # Dotfiles for Aaron Boushley
-These are my basic configuration files an vim plugins.
+These are my basic configuration files and vim plugins.
--- setup_links.rb
+++ setup_links.rb
@@ -3,22 +3,13 @@
 require 'FileUtils'
 
 files_to_link = {
-	"~/.dotfiles/git/gitconfig" => "~/.gitconfig",
-	"~/.dotfiles/git/gitignore_global" => "~/.gitignore_global",
-	"~/.dotfiles/tmux/tmux.conf" => "~/.tmux.conf",
-	"~/.dotfiles/vim/vim" => "~/.vim",
-	"~/.dotfiles/vim/vimrc" => "~/.vimrc",
-	"~/.dotfiles/zsh/oh_my_zsh_rc" => "~/.zshrc",
+	"~/dotfiles/git/gitconfig" => "~/.gitconfig",
+	"~/dotfiles/git/gitignore_global" => "~/.gitignore_global",
 
-	"~/.dotfiles/git/gitconfig" => "~/.gitconfig",
-	"~/.dotfiles/git/gitignore_global" => "~/.gitignore_global",
+	"~/dotfiles/tmux/tmux.conf" => "~/.tmux.conf",
 
-	"~/.dotfiles/tmux/tmux.conf" => "~/.tmux.conf",
-
-	"~/.dotfiles/vim/vim" => "~/.vim",
-	"~/.dotfiles/vim/vimrc" => "~/.vimrc",
-
-	"~/.dotfiles/zsh/oh_my_zsh_rc" => "~/.zshrc"
+	"~/dotfiles/vim/vim" => "~/.vim",
+	"~/dotfiles/vim/vimrc" => "~/.vimrc",
 }
 
 files_to_link.each do |source, target|
@@ -34,4 +25,20 @@ files_to_link.each do |source, target|
 	end
 
 	FileUtils.ln_s(source, target)
+end
+
+files_to_create = {
+	 "~/.zshrc" => "source ~/dotfiles/zsh/oh_my_zsh_rc\n\n# Customize the path as you need\nexport PATH=$PATH\n\n# Import ssh identities for this machine\nssh-add ~/.ssh/id_rsa",
+   "~/.aliases" => "source ~/dotfiles/zsh/aliases"
+}
+
+files_to_create.each do |target, content|
+	target = File.expand_path(target)
+
+	if File.exists? target
+		puts "Skipping #{target} since a file already exists there"
+		next
+	end
+
+  File.open(target, 'w') { |file| file.write(content) }
 end
