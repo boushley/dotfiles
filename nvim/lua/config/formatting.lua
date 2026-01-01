99fe12df8c6d20ff3e947c7105054776af9bcd5c
Aaron Boushley <aaron@boushley.cc>
2026-01-01 09:01:48 -0800

    Set new formatting defaults

--- nvim/lua/config/formatting.lua
+++ nvim/lua/config/formatting.lua
@@ -1,3 +1,6 @@
-vim.cmd 'set expandtab'
-vim.cmd 'set tabstop=2'
-vim.cmd 'set shiftwidth=2'
+vim.opt.expandtab = true    -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4      -- Size of an indent
+vim.opt.tabstop = 4         -- Number of spaces tabs count for
+vim.opt.softtabstop = 4     -- Insert 4 spaces for a tab
+vim.opt.autoindent = true   -- Copy indent from current line when starting new one
+vim.opt.smartindent = false -- Don't use the built in smartindent, rely on TreeSitter instead
