8e0d9a45132b48d9fa935221258e2902a5f48a60
Aaron Boushley <aaron@boushley.cc>
2026-01-25 12:19:29 -0800

    Get processing bindings working well

--- nvim/lua/config/processing.lua
+++ nvim/lua/config/processing.lua
@@ -18,7 +18,31 @@ vim.api.nvim_create_autocmd("FileType", {
     -- Set the error format to enable jumping to code lines from the Quickfix window
     vim.opt_local.errorformat = "%f:%l:%c:%*\\d:%*\\d:%t%*[^:]:%m"
 
-    -- Optional: Add a keymap specifically for this filetype to run it easily
-    vim.keymap.set('n', '<leader>r', ':split | term processing-java --sketch=%:p:h --force --run<CR>', { buffer = true })
+
+    -- Global variable to track the Processing terminal buffer
+    _G.processing_term_buf = nil
+
+    local function run_processing_sketch()
+      -- 1. Close existing terminal buffer if it's still open
+      if _G.processing_term_buf and vim.api.nvim_buf_is_valid(_G.processing_term_buf) then
+        -- force = true ensures that we can close the buffer even if the command is still running
+        vim.api.nvim_buf_delete(_G.processing_term_buf, { force = true })
+      end
+
+      -- 2. Define the command
+      local sketch_path = vim.fn.expand('%:p:h')
+      local cmd = string.format("processing-java --sketch=%s --force --run", sketch_path)
+
+      -- 3. Open a split and start the terminal
+      local height = math.floor(vim.o.lines * 0.20)
+      vim.cmd(height .. "split")
+      vim.cmd("term " .. cmd)
+
+      -- 4. Save the new buffer ID and set it to auto-scroll/cleanup
+      _G.processing_term_buf = vim.api.nvim_get_current_buf()
+    end
+
+    -- Update your keymap inside the Autocmd callback:
+    vim.keymap.set('n', '<leader>r', run_processing_sketch, { buffer = true, desc = "Run Processing Sketch" })
   end,
 })
