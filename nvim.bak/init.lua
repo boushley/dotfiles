bc00dac8a0ac86c8a2d6f9efed016ca422a67382
Aaron Boushley <aaron.boushley@gm.com>
2026-07-06 14:12:50 -0700

    Switch to mostly stock LazyVim for config

---
+++ nvim.bak/init.lua
@@ -0,0 +1,20 @@
+require('config.initial')
+require('config.options')
+require('config.keymaps')
+require('config.auto-commands')
+require('config.directories')
+require('config.processing')
+
+-- This is useful on some systems where OSC52 works well, otherwise the universal-clipboard plugin is nice
+require('config.clipboard')
+require('config.lazy')
+require('config.formatting')
+require('config.prose')
+
+require('lazy').setup('plugins')
+
+-- Override plugin that sets clipboard to unnamedplus since I prefer the default behavior where
+-- standard deletes, etc in vim overwrite the system buffer.
+vim.opt.clipboard = ""
+
+require('config.theme')
